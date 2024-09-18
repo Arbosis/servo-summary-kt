bcfdba7511a54988695d1842340300e8909edd77
Arbosis <kdahlskog@gmail.com>
2024-09-18 23:19:14 +0200

    style 2 looks good now

--- src/ToggleBox.js
+++ src/ToggleBox.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ToggleBox = ({ text1, text2, onChangeHandle }) => {
     return (
         <>
-        <input type="checkbox" onChange={onChangeHandle} id="toggle" class="toggleCheckbox" />
-            <label for="toggle" class='toggleContainer'>
+        <input type="checkbox" onChange={onChangeHandle} id="toggle" className="toggleCheckbox" />
+            <label htmlFor="toggle" className='toggleContainer'>
             <div>{text1}</div>
             <div>{text2}</div>
         </label>
